bb5a23531e543147b460503c9c31f6df714b170e
Junaid Iqbal <Junaid.jigar761@gmail.com>
2022-10-04 17:49:41 +0500

    T06.02 MakeAPreferenceFragment : pref_visualizer.xml added

--- app/src/main/java/android/example/com/visualizerpreferences/SettingsFragment.java
+++ app/src/main/java/android/example/com/visualizerpreferences/SettingsFragment.java
@@ -1,4 +1,13 @@
 package android.example.com.visualizerpreferences;
 
-class SettingsFragment {
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.preference.PreferenceFragmentCompat;
+
+class SettingsFragment extends PreferenceFragmentCompat {
+    @Override
+    public void onCreatePreferences(@Nullable Bundle savedInstanceState, @Nullable String rootKey) {
+        addPreferencesFromResource(R.xml.pref_visualizer);
+    }
 }
