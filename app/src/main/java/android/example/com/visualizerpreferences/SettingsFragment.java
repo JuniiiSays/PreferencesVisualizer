4bf27707da455a7301a4ed2d214c87a2af63df8d
Junaid Iqbal <Junaid.jigar761@gmail.com>
2022-10-04 18:33:03 +0500

    T06.02 MakeAPreferenceFragment : Setting Screen working fine

--- app/src/main/java/android/example/com/visualizerpreferences/SettingsFragment.java
+++ app/src/main/java/android/example/com/visualizerpreferences/SettingsFragment.java
@@ -5,7 +5,11 @@ import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.preference.PreferenceFragmentCompat;
 
-class SettingsFragment extends PreferenceFragmentCompat {
+public class SettingsFragment extends PreferenceFragmentCompat {
+
+    public SettingsFragment() {
+    }
+
     @Override
     public void onCreatePreferences(@Nullable Bundle savedInstanceState, @Nullable String rootKey) {
         addPreferencesFromResource(R.xml.pref_visualizer);
